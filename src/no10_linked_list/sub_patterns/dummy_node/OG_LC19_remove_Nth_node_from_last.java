bcd83a229861e35fa58ca3fbd5dd549935c6694d
Sneha <sneha2000kakri@gmail.com>
2025-12-16 13:51:28 +0530

    Lc19,LC21

--- src/no10_linked_list/sub_patterns/dummy_node/OG_LC19_remove_Nth_node_from_last.java
+++ src/no10_linked_list/sub_patterns/dummy_node/OG_LC19_remove_Nth_node_from_last.java
@@ -3,7 +3,75 @@ import no10_linked_list.sub_patterns.ListNode;
 
 public class OG_LC19_remove_Nth_node_from_last {
 
+    public ListNode removeNthFromEndBruteForce(ListNode head, int n) {
+        int len = 0;
+        ListNode curr = head;
+
+        while (curr != null) {
+            len++;
+            curr = curr.next;
+        }
+
+        int idx = len - n;
+        if (idx == 0) return head.next;
+
+        curr = head;
+        for (int i = 1; i < idx; i++) {
+            curr = curr.next;
+        }
+        curr.next = curr.next.next;
+        return head;
+    }
+
+    public ListNode removeNthFromEndBetterApproach(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) return head.next;
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+
     public static void main(String[] args) {
+        OG_LC19_remove_Nth_node_from_last obj = new OG_LC19_remove_Nth_node_from_last();
+        // head = [1,2,3,4,5]
+        ListNode n1= new ListNode(1);
+        ListNode n2= new ListNode(2);
+        ListNode n3= new ListNode(3);
+        ListNode n4= new ListNode(4);
+        ListNode n5= new ListNode(5);
+        ListNode n6= new ListNode(6);
+
+        // link nodes: 1->2->3->4
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+        ListNode head = n1;
+        int n =2;
+
+//        System.out.print("Brute force: ");
+//        ListNode bruteResult = obj.removeNthFromEndBetterApproach(head,n);
+//        ListNode.printList(bruteResult);
+
+        System.out.print("Better Sol: ");
+        ListNode optimisedResult = obj.removeNthFromEndBetterApproach(head,n);
+        ListNode.printList(optimisedResult);
+
+
+
+
 
     }
 }
--- src/no10_linked_list/sub_patterns/merge/OG_LC21_merge_2_sorted_list.java
+++ src/no10_linked_list/sub_patterns/merge/OG_LC21_merge_2_sorted_list.java
@@ -2,4 +2,91 @@ package no10_linked_list.sub_patterns.merge;
 import no10_linked_list.sub_patterns.ListNode;
 
 public class OG_LC21_merge_2_sorted_list {
+
+    public ListNode mergeTwoListsBruteForce(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+
+        while (p1 != null && p2 != null) {
+            if (p1.val <= p2.val) {
+                curr.next = new ListNode(p1.val);
+                p1 = p1.next;
+            } else {
+                curr.next = new ListNode(p2.val);
+                p2 = p2.next;
+            }
+            curr = curr.next;
+        }
+
+        while (p1 != null) {
+            curr.next = new ListNode(p1.val);
+            p1 = p1.next;
+            curr = curr.next;
+        }
+
+        while (p2 != null) {
+            curr.next = new ListNode(p2.val);
+            p2 = p2.next;
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+
+    public ListNode mergeTwoListsBetterApproach(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (list1 != null) tail.next = list1;
+        if (list2 != null) tail.next = list2;
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        OG_LC21_merge_2_sorted_list obj = new OG_LC21_merge_2_sorted_list();
+
+        // list1 = [1,2,4]
+        ListNode l1n1 = new ListNode(1);
+        ListNode l1n2 = new ListNode(3);
+        ListNode l1n3 = new ListNode(5);
+
+        l1n1.next = l1n2;
+        l1n2.next = l1n3;
+
+        ListNode list1 = l1n1;
+
+// list2 = [1,3,4]
+        ListNode l2n1 = new ListNode(2);
+        ListNode l2n2 = new ListNode(4);
+        ListNode l2n3 = new ListNode(6);
+
+        l2n1.next = l2n2;
+        l2n2.next = l2n3;
+
+        ListNode list2 = l2n1;
+
+// print to verify
+        ListNode.printList(list1);
+        ListNode.printList(list2);
+
+        System.out.print("Brute force: ");
+        ListNode bruteResult = obj.mergeTwoListsBruteForce(list1, list2);
+        ListNode.printList(bruteResult);
+
+    }
 }
